99f1de46ddc868d498fa180046754af82656b8b3
Furkan Titiz <furkantitiz96@gmail.com>
2025-06-28 14:54:18 +0300

    change break main content into smaller paragraphs for better UX

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
       
       <main className="main-content">
         <p>
-          I’m currently working on frontend projects and sharing them on<a href='https://github.com/qrphy' target='_blank'> GitHub</a>.
+          I’m currently working on frontend projects and sharing them on<a href='https://github.com/qrphy' target='_blank'> GitHub</a>.</p>
+          <p>
           Feel free to check them out and reach out for <a href="mailto:furkantitiz96@gmail.com">collaboration</a>.</p>
           <p>
           Outside of coding, I enjoy watching F1 races, tennis matches, and listening to music.
