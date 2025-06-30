cf059d264fc6b8f5fa9c09d9b3299af773c94b15
Furkan Titiz <furkantitiz96@gmail.com>
2025-06-30 22:11:57 +0300

    add hover tooltips fpr name link and optimize CSS

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import Projects from "./components/Projects";
 import "./App.css";
+
+function HeaderContent() {
+  const location = useLocation();
+  const tooltipText = location.pathname === "/" ? "about" : "home";
+  
+  return (
+    <div className="name">
+      <Link 
+        to={location.pathname === "/" ? "/about" : "/"} 
+        className="home-link"
+        data-tooltip={tooltipText}
+      >
+        <h1>furkan titiz</h1>
+      </Link>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="header">
-          <div className="name">
-            <Link to="/" className="home-link">
-              <h1>furkan titiz</h1>
-            </Link>
-          </div>
-          <nav className="nav-links">
-            <Link to="/about">about</Link>
-            <Link to="/projects">projects</Link>
-          </nav>
+          <HeaderContent />
         </header>
-
+        
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
           </Routes>
         </main>
       </div>
