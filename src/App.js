933824ec50b6d59d28cc0decc415cc53dc86a637
Furkan Titiz <furkantitiz96@gmail.com>
2025-07-01 17:24:21 +0300

    simplify homepage: remove about page, add connect section and artwork

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About";
 import "./App.css";
 
-function HeaderContent() {
-  const location = useLocation();
-  const tooltipText = location.pathname === "/" ? "about" : "home";
-  
-  return (
-    <div className="name">
-      <Link 
-        to={location.pathname === "/" ? "/about" : "/"} 
-        className="home-link"
-        data-tooltip={tooltipText}
-      >
-        <h1>furkan titiz</h1>
-      </Link>
-    </div>
-  );
-}
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="header">
-          <HeaderContent />
+          <div className="name">
+              <h1>Furkan Titiz</h1>
+          </div>
         </header>
         
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
           </Routes>
         </main>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -19,10 +19,23 @@ function Home() {
         Feel free to check them out and reach out for{" "}
         <a href="mailto:furkantitiz96@gmail.com">collaboration</a>.
       </p>
+      <br />
       <p>
         Outside of coding, I enjoy watching F1 races, tennis matches, and
         listening to music.
       </p>
+      <h4>Contact</h4>
+      <p>
+        You can reach me via email at{" "}
+        <a href="mailto:furkantitiz96@gmail.com">furkantitiz96@gmail.com</a>,
+        or catch me on{" "}
+        <a href="https://www.linkedin.com/in/furkan-titiz/">LinkedIn</a>.
+      </p>
+      
+      <div className="home-image">
+          <img src="/youngknight.jpg" alt="Young Knight by Vittore Carpaccio 1510" className="portrait-img" />
+        <p>Young Knight by Vittore Carpaccio 1510</p>
+      </div>
     </>
   );
 }
