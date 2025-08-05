c65fb6e082080322e4391eb9961b35b0812012b2
Furkan Titiz <curny999@gmail.com>
2025-08-05 03:46:43 +0300

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,26 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const text = "// redesign in progress...";
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < text.length) {
+      const timeout = setTimeout(() => {
+        setIndex(index + 1);
+      }, 100);
+      return () => clearTimeout(timeout);
+    }
+  }, [index, text.length]);
+
   return (
     <div className="text-white h-[100vh] bg-black">
       <div className="flex flex-col items-center justify-center h-full">
-        <Image
-          src="/YoungKnight.jpg"
-          style={{
-            width: "300px",
-            borderRadius: "15px",
-            boxShadow: "0 0 10px rgba(255, 255, 255, 0.5)",
-            filter: "grayscale(40%)",
-          }}
-          alt="Young Knight"
-        />
-        <p className="fixed whitespace-nowrap text-xl pointer-events-none justify-center items-center flex w-full h-full">
-          // rebuilding in progress...
+        <p className="fixed whitespace-nowrap text-xl pointer-events-none justify-center items-center flex w-full h-full font-system">
+          {text.slice(0, index)}
         </p>
       </div>
     </div>
   );
-
-  /*
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p className="hover:underline">coming soon...</p>
-      </main>
-    </div> */
 }
