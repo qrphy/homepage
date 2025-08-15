be2ac16316127baa5d1f15abb01371a52cccedac
Furkan Titiz <curny999@gmail.com>
2025-08-15 22:46:17 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,40 @@
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black text-white p-8">
-      <div className="max-w-2xl mx-auto">
-        <h1 className="text-3xl font-bold mb-8">Furkan Titiz</h1>
-        <p className="mb-4 text-lg">
+    <div className="f-full bg-black text-white pt-16 m-6">
+      <div className="max-w-lg mx-auto">
+        <h1 className="text-xl font-semibold mb-6">Furkan Titiz</h1>
+        <p className="mb-3 text-base">
           I'm currently working on frontend projects and sharing them on
           <a
             href="https://github.com/qrphy"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-400 hover:text-blue-300 underline ml-1"
+            className="hover:text-blue-500 underline ml-1"
           >
             GitHub
           </a>
           .
         </p>
-        <p className="mb-4 text-lg">
+        <p className="mb-3 text-base">
           Feel free to check them out and reach out for{" "}
           <a
             href="mailto:furkantitiz96@gmail.com"
-            className="text-blue-400 hover:text-blue-300 underline"
+            className="hover:text-blue-500 underline"
           >
             collaboration
           </a>
           .
         </p>
-        <p className="mb-6 text-lg">
+        <p className="mb-12 text-base">
           Outside of coding, I enjoy watching F1 races, tennis matches, and
           listening to music.
         </p>
-        <h4 className="text-xl font-semibold mb-4">Contact</h4>
-        <p className="text-lg">
-          Catch me on
+        <h4 className="text-xl font-semibold mb-3">Contact</h4>
+        <p className="text-base">
+          Catch me on{" "}
           <a
             href="https://www.linkedin.com/in/furkan-titiz/"
-            className="text-blue-400 hover:text-blue-300 underline ml-1"
+            className="hover:text-blue-500 underline"
           >
             LinkedIn
           </a>
