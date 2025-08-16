3a714703313c2a59e6706ba8627b29044581254a
Furkan Titiz <curny999@gmail.com>
2025-08-17 00:20:07 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,41 +3,54 @@ export default function Home() {
     <div className="f-full bg-black text-white pt-16 m-6">
       <div className="max-w-sm mx-auto">
         <header className="mb-6">
-          <h1 className="text-sm font-semibold">Furkan Titiz</h1>
-          <p className="text-xs font-medium text-gray-500">
+          <h1 className="text-base font-semibold">Furkan Titiz</h1>
+          <p className="text-xs font-semibold text-gray-400">
             Frontend Developer
           </p>
         </header>
-        <div className="mb-12 [&>p]:leading-loose text-sm font-normal">
+        <div className="mb-12 [&>p]:leading-snug text-sm font-medium">
           <p>
-            I focus on developing frontend projects using JavaScript,
-            TypeScript, React, Next.js, and TailwindCSS.
+            I build digital products that transform ideas into effortless and
+            enjoyable web experiences.
           </p>
-
+          <br />
           <p>
-            Outside of coding, I enjoy watching F1 races, tennis matches, and
-            listening to music.
+            I graduated from Selcuk University with a degree in Computer
+            Programming.
+          </p>
+          <br />
+          <p>
+            I&apos;m currently working on frontend project and sharing them on{" "}
+            <a
+              href="https://github.com/qrphy"
+              className="underline hover:no-underline"
+            >
+              GitHub
+            </a>
+            .
           </p>
         </div>
-        <h4 className="mb-3 text-sm font-semibold">Contact</h4>
-        <p className="text-sm">
-          Drop me an{" "}
-          <a
-            href="mailto:furkantitiz44@gmail.com"
-            className="underline hover:no-underline"
-          >
-            {""}email
-          </a>{" "}
-          or catch me on{" "}
-          <a
-            href="https://www.linkedin.com/in/furkan-titiz/"
-            className="underline hover:no-underline"
-          >
-            {" "}
-            LinkedIn
-          </a>
-          .
-        </p>
+        <div>
+          <h4 className="mb-3 text-base font-semibold">Contact</h4>
+          <p className="text-sm">
+            Drop me an{" "}
+            <a
+              href="mailto:furkantitiz44@gmail.com"
+              className="underline hover:no-underline"
+            >
+              {""}email
+            </a>{" "}
+            or catch me on{" "}
+            <a
+              href="https://www.linkedin.com/in/furkan-titiz/"
+              className="underline hover:no-underline"
+            >
+              {" "}
+              LinkedIn
+            </a>
+            .
+          </p>
+        </div>
       </div>
     </div>
   );
