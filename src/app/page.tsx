3088414a3b10a73636bb8ea6f8188e59ec99522b
Furkan Titiz <curny999@gmail.com>
2025-08-16 11:51:24 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,38 @@ export default function Home() {
   return (
     <div className="f-full bg-black text-white pt-16 m-6">
       <div className="max-w-lg mx-auto">
-        <h1 className="text-xl font-semibold mb-6">Furkan Titiz</h1>
-        <p className="mb-3 text-base">
-          I&apos;m currently working on frontend projects and sharing them on
-          <a
-            href="https://github.com/qrphy"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-blue-500 underline ml-1"
-          >
-            GitHub
-          </a>
-          .
-        </p>
-        <p className="mb-3 text-base">
-          Feel free to check them out and reach out for{" "}
-          <a
-            href="mailto:furkantitiz96@gmail.com"
-            className="hover:text-blue-500 underline"
-          >
-            collaboration
-          </a>
-          .
-        </p>
-        <p className="mb-12 text-base">
-          Outside of coding, I enjoy watching F1 races, tennis matches, and
-          listening to music.
-        </p>
+        <header className="mb-6">
+          <h1 className="text-xl font-semibold">Furkan Titiz</h1>
+          <p className="text-xs text-base">Frontend Developer</p>
+        </header>
+        <div className="mb-12 [&>p]:leading-loose">
+          <p className="text-base">
+            I&apos;m currently working on frontend projects and sharing them on
+            <a
+              href="https://github.com/qrphy"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-500 underline ml-1"
+            >
+              GitHub
+            </a>
+            .
+          </p>
+          <p className="text-base">
+            Feel free to check them out and reach out for{" "}
+            <a
+              href="mailto:furkantitiz96@gmail.com"
+              className="hover:text-blue-500 underline"
+            >
+              collaboration
+            </a>
+            .
+          </p>
+          <p className="text-base">
+            Outside of coding, I enjoy watching F1 races, tennis matches, and
+            listening to music.
+          </p>
+        </div>
         <h4 className="text-xl font-semibold mb-3">Contact</h4>
         <p className="text-base">
           Catch me on{" "}
