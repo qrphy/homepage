49598b23b216237cde1bf43364a42283defafbbc
Furkan Titiz <curny999@gmail.com>
2025-08-26 16:07:19 +0300

    fix: replace <a> with next/link for internal routes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,18 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="w-full pt-3">
       <div className="flex justify-between items-center text-lg font-medium text-sm">
         <div>
-          <a href="/">Furkan Titiz</a>
+          <Link href="/">Furkan Titiz</Link>
           <p className="text-xs font-medium text-gray-400">
             Frontend Developer
           </p>
         </div>
         <div className="flex justify-between items-center gap-4">
-          <a href="/projects">Projects</a>
-          <a href="/bookmarks">Bookmarks</a>
+          <Link href="/projects">Projects</Link>
+          <Link href="/bookmarks">Bookmarks</Link>
         </div>
       </div>
     </header>
