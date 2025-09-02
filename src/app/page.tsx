d50e5418845799eb37eafaaf74f67613c67bbc8f
Furkan Titiz <curny999@gmail.com>
2025-09-03 01:44:11 +0300

    update main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className="underline underline-offset-2 hover:text-blue-600 transition-colors duration-200"
           >
-            .
+            GitHub.
           </a>{" "}
           Feel free to take a look.
         </>
