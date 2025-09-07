2b20fcf4c598474c12983de2a36fd1ec9726cf9a
Furkan Titiz <curny999@gmail.com>
2025-09-07 15:33:55 +0300

    update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,22 @@
 export default function Footer() {
   return (
-    <footer className="mt-auto py-8 text-center text-base text-gray-600 dark:text-gray-400 ct-color">
-      <div className="flex justify-between items-center gap-4 px-4">
-        <a
-          href="/bookmarks"
-          className="hover:underline hover:text-blue-600 underline-offset-2 transition-colors duration-200"
-          aria-label="Go to bookmarks page"
-        >
-          Bookmarks
-        </a>
+    <footer className="mt-auto py-8 text-center text-base ct-color">
+      <div className="flex justify-between items-center">
+        <div className="flex justifiy-between items-center gap-4 text-base font-normal">
+          <a
+            href="/bookmarks"
+            className="hover:underline hover:text-blue-600 underline-offset-2 transition-colors duration-200"
+            aria-label="Go to bookmarks page"
+          >
+            Bookmarks
+          </a>
+          <a
+            href="/films"
+            className="hover:underline hover:text-blue-600 underline-offset-2 transition-colors duration-200"
+          >
+            Films
+          </a>
+        </div>
         <p>&copy; 2025</p>
       </div>
     </footer>
