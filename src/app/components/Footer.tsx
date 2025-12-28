df63854b84c0c7b34c18c672b67ca603a52618b2
furkan <curny999@gmail.com>
2025-12-28 11:26:51 +0300

    update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,16 +6,16 @@ export default function Footer() {
     <footer className="mt-auto pt-1 pb-1 bg-black text-sm rounded-4xl -translate-y-14 max-w-sm mx-auto px-4 shadow-lg shadow-gray-900/60 text-white underline-offset-3 md:pt-2 md:pb-2 md:text-md md:max-w-md">
       <div className="flex justify-around items-center gap-4 md:gap-6">
         <Link href="/" target="_self" rel="noopener noreferrer">
-          <p className="hover:underline">home</p>
+          <p className="hover:underline">Home</p>
         </Link>
         <Link href="https://github.com/qrphy" target="_blank" rel="noopener noreferrer">
-          <p className="hover:underline">projects</p>
+          <p className="hover:underline">Projects</p>
         </Link>
         <Link href="/bookmarks" target="_self" rel="noopener noreferrer">
-          <p className="hover:underline">bookmarks</p>
+          <p className="hover:underline">Bookmarks</p>
         </Link>
         <Link href="https://www.linkedin.com/in/furkan-titiz/" target="_blank" rel="noopener noreferrer">
-          <p className="hover:underline">contact</p>
+          <p className="hover:underline">Contact</p>
         </Link>
         <Link href="https://www.x.com/qrphy" target="_blank" rel="noopener noreferrer">
         <Image className="rounded-full cursor-pointer hover:brightness-90 transition-opacity w-8 h-8 md:w-10 md:h-10"
