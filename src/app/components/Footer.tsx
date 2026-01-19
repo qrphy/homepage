e6a5254a0ea5ff9a027abe64441905423dd76a16
furkan <curny999@gmail.com>
2026-01-19 11:27:50 +0300

    update ui

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="mt-auto mb-10">
-      <div className="flex justify-around items-center font-base italic text-xs">
+      <div className="flex justify-around items-center font-base italic text-xs ct-color">
         <p>trying to be better than yesterday</p>
       </div>
     </footer>
