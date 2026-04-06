2172bb331df6a45eac1af8c80f1399979c868388
furkan <curny999@gmail.com>
2026-04-06 11:27:23 +0300

    update: projects.tsx

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -5,24 +5,13 @@ export default function Projects() {
       <section className="w-full">  
         <p>Currently building{" "}
         <a
-            href="https://aurora-lamb.vercel.app"
+            href="https://stylefinden.com"
             target="_blank"
             rel="noopener noreferrer"
             className="aurora-link decoration-current/40 hover:decoration-(--my-color) text-blue-500 underline-offset-3 transition-colors duration-200"
           >
-            Aurora Lamb
-          </a>
-          {" "}
-          and
-          <a
-          href="https://eclat-iota.vercel.app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="aurora-link decoration-current/40 hover:decoration-(--my-color) text-blue-500 underline-offset-3 transition-colors duration-200"
-        >
-          {" "}
-          ÉCLAT
-            </a>. My
+            STYLEFINDEN
+          </a>. My
           projects can be found on{" "}
           <a
             href="https://github.com/qrphy"
